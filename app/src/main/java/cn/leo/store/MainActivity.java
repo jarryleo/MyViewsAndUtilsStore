b89f7cb53781c8666bde5ace3c19b5e1f2ec6de7
liujiarui <liujiarui@ship56.net>
2019-02-26 14:31:04 +0800

    新增安全handler类

--- app/src/main/java/cn/leo/store/MainActivity.java
+++ app/src/main/java/cn/leo/store/MainActivity.java
@@ -1,11 +1,12 @@
 package cn.leo.store;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.TouchDelegate;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
 
 import cn.leo.store.utils.EditTextNextFocusUtil;
+import cn.leo.store.utils.SafetyMainHandler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         EditTextNextFocusUtil.autoBind(this);
+        SafetyMainHandler safetyMainHandler = new SafetyMainHandler(this, new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+
+                return false;
+            }
+        });
+
+
     }
 }
